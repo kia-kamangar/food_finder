20e6b412acdda65c2b7b619aa9dbabd865183ab4
Kia Kamangar <kia.kamangar@gmail.com>
2019-06-11 14:08:46 +0430

    Add init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+# FOOD FINDER
+APP_ROOT = File.dirname(__FILE__)
+
+require "#{APP_ROOT}/lib/guide"
+#require  File.join(APP_ROOT, 'lib', 'guide')
+# require File.join(APP_ROOT, 'lib', 'guide')
+#$:.unshift(File.join(APP_ROOT, 'lib'))
+#require 'guide'
