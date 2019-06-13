ef682d42037a9d66217dea76231387bc51c1b090
Kia Kamangar <kia.kamangar@gmail.com>
2019-06-13 13:27:30 +0430

    Add init file directory

--- init.rb
+++ init.rb
@@ -6,3 +6,6 @@ require "#{APP_ROOT}/lib/guide"
 # require File.join(APP_ROOT, 'lib', 'guide')
 #$:.unshift(File.join(APP_ROOT, 'lib'))
 #require 'guide'
+guide = Guide.new('restaurant.txt')
+
+guide.launch!
